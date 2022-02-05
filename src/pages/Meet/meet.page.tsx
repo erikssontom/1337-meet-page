f0e35509fd4803c7fae577245d510e0eb57d55fb
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-05 23:41:42 +0100

    Moved fetching of employees to meet page

--- src/pages/Meet/meet.page.tsx
+++ src/pages/Meet/meet.page.tsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Employee } from "types";
 import Grid from "../../components/Grid";
 import ToolBar from "../../components/ToolBar";
 
 const Meet = () => {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const endpoint = process.env.REACT_APP_API_ENDPOINT;
+    const fetchData = async (apiKey: string, endpoint: string) => {
+      const res = await fetch(endpoint, {
+        headers: { Authorization: apiKey },
+      });
+      const json = await res.json();
+      return json;
+    };
+    if (!apiKey || !endpoint) return;
+    // fetchData(apiKey, endpoint).then(console.log);
+  }, []);
   return (
     <div>
-      Meet
       <ToolBar />
       <Grid />
     </div>
