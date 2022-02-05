cb54445d5172bea74683f6c1101f946deb6cc69b
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-05 23:13:51 +0100

    Added employee card component

--- src/components/EmployeeCard/employeeCard.component.test.tsx
+++ src/components/EmployeeCard/employeeCard.component.test.tsx
@@ -2,8 +2,39 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import EmployeeCard from ".";
 
-test("renders EmployeeCard component", () => {
-  render(<EmployeeCard />);
-  const employeeCardText = screen.getByText("EmployeeCard");
-  expect(employeeCardText).toBeInTheDocument();
+const testData = {
+  name: "Agron Kabashi",
+  office: "Lund",
+  gitHub: "AgronKabashi",
+  twitter: "_agronkabashi",
+  linkedIn: "/pub/agron-kabashi/54/6a8/4a6",
+  imagePortraitUrl: "https://i.1337co.de/profile/agron-kabashi",
+};
+
+it("renders correct text", () => {
+  render(<EmployeeCard {...testData} />);
+  const name = screen.getByText(testData.name);
+  const office = screen.getByText(`Office: ${testData.office}`);
+  expect(name).toBeInTheDocument();
+  expect(office).toBeInTheDocument();
+});
+
+it("renders social links", () => {
+  render(<EmployeeCard {...testData} />);
+  const socialLinks = screen.getAllByRole("link");
+
+  const amountOfSocials = [
+    testData.gitHub,
+    testData.linkedIn,
+    testData.twitter,
+  ].filter((handle) => handle).length;
+
+  expect(amountOfSocials).toBe(socialLinks.length);
+});
+
+it("renders image", () => {
+  render(<EmployeeCard {...testData} />);
+  const img = screen.getByAltText(testData.name);
+  expect(img).toBeInTheDocument();
+  expect(img.getAttribute("src")).toBe(testData.imagePortraitUrl);
 });
--- src/components/EmployeeCard/employeeCard.component.tsx
+++ src/components/EmployeeCard/employeeCard.component.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { FC } from "react";
+import { Employee } from "types";
+import SocialIcon from "../SocialIcon";
+import styles from "./employeeCard.module.css";
 
-const EmployeeCard = () => {
-  return <div>EmployeeCard</div>;
+type EmployeeCardProps = Employee;
+
+const EmployeeCard: FC<EmployeeCardProps> = ({
+  name,
+  office,
+  gitHub,
+  twitter,
+  linkedIn,
+  imagePortraitUrl,
+}) => {
+  return (
+    <div className={styles.container}>
+      <img className={styles.img} src={imagePortraitUrl} alt={name} />
+      <div className={styles.cardInfoGrid}>
+        <div className={styles.cardInfoGridRow}>
+          <h4>{name}</h4>
+          <ul className={styles.socialIconList}>
+            {linkedIn && (
+              <li>
+                <SocialIcon socialCompany="linkedIn" handle={linkedIn} />
+              </li>
+            )}
+            {gitHub && (
+              <li>
+                <SocialIcon socialCompany="gitHub" handle={gitHub} />
+              </li>
+            )}
+            {twitter && (
+              <li>
+                <SocialIcon socialCompany="twitter" handle={twitter} />
+              </li>
+            )}
+          </ul>
+        </div>
+        <h4 className={styles.cardInfoGridRow}>Office: {office}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default EmployeeCard;
