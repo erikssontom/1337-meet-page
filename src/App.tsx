7324f0d1a47c237eab9cf990ca6c1f8951d181bb
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-05 13:16:57 +0100

    Tested API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Meet from "./pages/Meet";
-
 const App = () => {
+  useEffect(() => {
+    const apiKey = process.env.REACT_APP_API_KEY;
+    const endpoint = process.env.REACT_APP_API_ENDPOINT;
+    const fetchData = async (apiKey: string, endpoint: string) => {
+      const res = await fetch(endpoint, {
+        headers: { Authorization: apiKey },
+      });
+      const json = await res.json();
+      return json;
+    };
+    if (!apiKey || !endpoint) return;
+    fetchData(apiKey, endpoint).then(console.log);
+  }, []);
   return (
     <div>
       App
