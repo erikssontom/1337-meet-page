dd2bd3e7c38109094925218ba549c8b1405cfb16
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-05 23:06:17 +0100

    Added employee type

---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+declare module "types" {
+  export type Employee = {
+    name: string;
+    office: string;
+    gitHub?: string;
+    twitter?: string;
+    linkedIn?: string;
+    imagePortraitUrl: string;
+  };
+}
