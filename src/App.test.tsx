2e046f08d816e941701b9098002326792e839cfe
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-05 23:40:08 +0100

    Removed old test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,12 +4,8 @@ import App from "./App";
 
 test("renders App component", () => {
   render(<App />);
-  const appText = screen.getByText("App");
-  const meetPageText = screen.getByText("Meet");
   const toolbarInMeetPage = screen.getByText("Toolbar");
   const gridInMeetPage = screen.getByText("Grid");
-  expect(appText).toBeInTheDocument();
   expect(toolbarInMeetPage).toBeInTheDocument();
   expect(gridInMeetPage).toBeInTheDocument();
-  expect(meetPageText).toBeInTheDocument();
 });
--- src/pages/Meet/meet.page.test.tsx
+++ src/pages/Meet/meet.page.test.tsx
@@ -4,10 +4,8 @@ import Meet from "./";
 
 test("renders App component", () => {
   render(<Meet />);
-  const meetPageText = screen.getByText("Meet");
   const toolbarInMeetPage = screen.getByText("Toolbar");
   const gridInMeetPage = screen.getByText("Grid");
   expect(toolbarInMeetPage).toBeInTheDocument();
   expect(gridInMeetPage).toBeInTheDocument();
-  expect(meetPageText).toBeInTheDocument();
 });
