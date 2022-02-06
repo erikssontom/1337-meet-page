2989fd84d1bf20f0e31b78d515b7f6e7121a4782
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-06 16:57:31 +0100

    Added meet test

--- src/pages/Meet/meet.page.test.tsx
+++ src/pages/Meet/meet.page.test.tsx
@@ -1,7 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Meet from "./";
 
-test("renders App component", () => {
+
+
+it("renders Toolbar component in Meet", () => {
   render(<Meet />);
+  const toolBarSortOn = screen.getByText('Sort on:');
+  const toolBarFilterOn = screen.getByText('Filter on:');
+  const toolBarOffice = screen.getByText('Office:');
+  const toolBarName = screen.getByText('Name:');
+
+  [toolBarSortOn, toolBarFilterOn, toolBarOffice, toolBarName].forEach(elem => expect(elem).toBeInTheDocument())
+});
+
+it("renders cards in Meet", async () => {
+  render(<Meet />);
+  const allCards = await screen.findAllByTestId('employee-card');
+  expect(allCards.length).toBeGreaterThan(0);
 });
