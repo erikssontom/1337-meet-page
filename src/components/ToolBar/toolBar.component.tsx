93754ded91cb6457bb70b2732738686733d7327d
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-06 15:19:19 +0100

    Added toolbar component

--- src/components/ToolBar/toolBar.component.tsx
+++ src/components/ToolBar/toolBar.component.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+import React, { Dispatch, FC, SetStateAction } from "react";
+import { FilterParam, SortParam } from "types";
+import styles from './toolBar.module.css';
 
-const ToolBar = () => {
-  return <div></div>;
+type ToolBarProps = {
+  setSortParam: Dispatch<SetStateAction<SortParam>>;
+  setFilterParam: Dispatch<SetStateAction<FilterParam>>;
+}
+
+const ToolBar: FC<ToolBarProps> = ({ setSortParam, setFilterParam }) => {
+  return <div className={styles.container}>
+    <div className={styles.toolBarColumn}>
+      Sort on:
+      <div className={styles.toolBarRow}>
+        <button onClick={() => {
+          setSortParam(({ method }) => { return { method: method * -1, var: 'office' } })
+        }} >Office</button>
+        <button onClick={() => {
+          setSortParam(({ method }) => { return { method: method * -1, var: 'name' } })
+        }} >Name</button>
+      </div>
+    </div>
+    <div className={styles.toolBarColumn}>
+      Filter on:
+      <div className={styles.toolBarRow}>
+        <label>
+          Office:
+          <input type="text" onChange={e => setFilterParam(({ name }) => { return { name, office: e.target.value } })} />
+        </label>
+        <label>
+          Name:
+          <input type="text" onChange={e => setFilterParam(({ office }) => { return { office, name: e.target.value } })} />
+        </label>
+      </div>
+    </div>
+  </div >;
 };
 
 export default ToolBar;
