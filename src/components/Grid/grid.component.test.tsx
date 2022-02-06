0a61b54c58325fe287a86b59afe7537b04cb9825
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-06 11:22:58 +0100

    Revert "Created test of grid" since duplicate test
    This reverts commit cc91ac9a2446fb6e07d6a909fe5c7937af43f7fd.

--- src/components/Grid/grid.component.test.tsx
+++ src/components/Grid/grid.component.test.tsx
@@ -21,22 +21,3 @@ it("renders Grid component containing employee cards", async () => {
   expect(employeeCards.length).toBe(employees.length);
   expect(employeeCards.length).toBeGreaterThan(0);
 });
-
-it("Employee cards are visible in grid", async () => {
-  const apiKey = process.env.REACT_APP_API_KEY;
-  const endpoint = process.env.REACT_APP_API_ENDPOINT;
-  const fetchData = async (apiKey: string, endpoint: string) => {
-    const res = await fetch(endpoint, {
-      headers: { Authorization: apiKey },
-    });
-    const json = await res.json();
-    return json;
-  };
-  if (!apiKey || !endpoint) return;
-  const employees = (await fetchData(apiKey, endpoint)) as Employee[];
-  if (!employees) return;
-  render(<Grid employees={employees} />);
-  const employeeCards = screen.getAllByTestId("employee-card");
-  expect(employeeCards.length).toBe(employees.length);
-  expect(employeeCards.length).toBeGreaterThan(0);
-});
