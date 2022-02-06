dc126d0f4a84b887deb4c5c6b8e22d56aaa05568
Tom Eriksson <tom.eriksson@hyperstack.se>
2022-02-06 16:06:57 +0100

    Moved up useEffect to top

--- src/pages/Meet/meet.page.tsx
+++ src/pages/Meet/meet.page.tsx
@@ -3,20 +3,13 @@ import { Employee, FilterParam, SortParam } from "types";
 import ErrorCard from "../../components/ErrorCard";
 import Grid from "../../components/Grid";
 import ToolBar from "../../components/ToolBar";
+import styles from './meet.module.css';
 
 const Meet = () => {
   const [employees, setEmployees] = useState<Employee[]>();
   const [hasErrorMessage, setHasErrorMessage] = useState(false);
   const [filterParam, setFilterParam] = useState<FilterParam>({ name: '', office: '' });
   const [sortParam, setSortParam] = useState<SortParam>({ method: -1, var: 'name' });
-
-  const sortedAndFilteredEmployees = useMemo(() => {
-    return employees?.filter(employee => {
-      return employee.name.toLowerCase().startsWith(filterParam.name.toLowerCase()) && employee.office.toLowerCase().startsWith(filterParam.office.toLowerCase());
-    }).sort((eA, eB) => {
-      return eA[sortParam.var] < eB[sortParam.var] ? sortParam.method : sortParam.method * -1;
-    })
-  }, [filterParam, sortParam, employees])
 
   useEffect(() => {
     const apiKey = process.env.REACT_APP_API_KEY;
@@ -41,9 +34,22 @@ const Meet = () => {
       setEmployees(data as Employee[]);
     });
   }, []);
+
+  const sortedAndFilteredEmployees = useMemo(() => {
+    return employees?.filter(employee => {
+      return employee.name.toLowerCase().startsWith(filterParam.name.toLowerCase()) && employee.office.toLowerCase().startsWith(filterParam.office.toLowerCase());
+    }).sort((eA, eB) => {
+      return eA[sortParam.var] < eB[sortParam.var] ? sortParam.method : sortParam.method * -1;
+    })
+  }, [filterParam, sortParam, employees])
+
   return (
-    <div>
-      <ToolBar setFilterParam={setFilterParam} setSortParam={setSortParam} />
+    <div className={styles.container}>
+      <div>
+        <h1 className={styles.meetHeading}>The fellowship of the tretton37</h1>
+        <ToolBar setFilterParam={setFilterParam} setSortParam={setSortParam} />
+      </div>
+
       <>
         {sortedAndFilteredEmployees && <Grid employees={sortedAndFilteredEmployees} />}
         {hasErrorMessage && !employees && <ErrorCard message="Oups! Seems like our team is not available right now! Please revisit us again soon!" />}
